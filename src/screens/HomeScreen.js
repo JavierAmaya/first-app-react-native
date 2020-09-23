fb703b97393885381dcdb4a416b5aeaab924b3e5
JavierAmaya <photojavhn@gmail.com>
2020-09-22 23:38:46 -0600

    chapter 4 ended

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,19 +2,19 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity} from "react-native";
  
 
-const HomeScreen = (props) => {
+const HomeScreen = (navigation) => {
   return <View>
     <Text style={styles.text}>Hi everyone! This is my first mobileApp, by Javier Amaya</Text>
     <Button 
-      onPress={ () => props.navigation.navigate('Components')}
+      onPress={ () => navigation.navigate('Components')}
       title="Go to components Demo"
     />
     <Button
-      onPress = { () => props.navigation.navigate('List')}
+      onPress = { () => navigation.navigate('List')}
       title = "Go to list since a easy button"
     />
     
-    <TouchableOpacity onPress={ () => props.navigation.navigate('List')} >
+    <TouchableOpacity onPress={ () => navigation.navigate('List')} >
       <Text> GO TO LIST DEMO From TouchableOpacity</Text>   
     </TouchableOpacity>
 
